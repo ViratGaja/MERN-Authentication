686f73a32fa2de07e2926517add4650112703fda
Gajapathi K <gajapathi787@gmail.com>
2025-03-15 21:29:24 +0530

    start

---
+++ Server/server.js
@@ -0,0 +1,16 @@
+import express from"express"
+import cors from "cors"
+import 'dotenv/config'
+import cookieParser from "cookie-parser"
+
+
+const app=express();
+const PORT=process.env.PORT || 4000
+
+app.use(express.json());
+app.use(cookieParser())
+app.use(cors({credentials:true}))
+app.listen(PORT,()=>{
+    console.log("Now Server is running");
+    
+})
