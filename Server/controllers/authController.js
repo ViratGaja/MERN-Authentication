441df42077f8818a2568f38cfd4bd2453571cfdc
Gajapathi K <gajapathi787@gmail.com>
2025-03-17 23:34:55 +0530

    learn

--- Server/controllers/authController.js
+++ Server/controllers/authController.js
@@ -37,6 +37,7 @@ export const register = async (req, res) => {
               sameSite:process.env.NODE_ENV === 'production' ? 'none' :"strict",
               maxAge:7*24*60*60*1000
             });
+            
           
             res.json({ success: true, message: 'Token generated successfully', token });
           } catch (error) {
@@ -47,4 +48,53 @@ export const register = async (req, res) => {
     catch (err) {
         res.json({ success: false, message: err.message })
     }
+}
+
+
+export const login=async (req,res)=>{
+
+  const {email,password}=req.body;
+
+  if(!email || !password){
+    return res.json({
+      success:false,message:error.message
+    })
+  }
+
+  try{
+    const user=await userModel.findOne({email});
+    if(!user){
+      return res.json({success:false,message:"Invalid email"})
+    }
+
+    const isMatch=await bcrypt.compare(password,user.password)
+    if(!isMatch){
+      return res.json({success:false,message:'Invalid password'})
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: '7d',
+    });
+  
+    res.cookie('token', token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite:process.env.NODE_ENV === 'production' ? 'none' :"strict",
+      maxAge:7*24*60*60*1000
+    });
+
+    return res.json({
+      success:true
+    })
+
+
+
+
+  }
+  catch(error){
+    return res.json({success:false,message:error.message})
+  }
+
+
+
+
 }
